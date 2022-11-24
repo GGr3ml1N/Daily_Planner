078c7bf2a4401919929b11107f1a6b2bb1188eca
GGr3ml1N <dinars163@gmail.com>
2022-11-25 01:40:19 +0400

    Переработан MainActivity и его разметка, добавлена нижняя панель навигации

--- app/src/main/java/com/ggr3ml1n/dailyplanner/activities/MainActivity.kt
+++ app/src/main/java/com/ggr3ml1n/dailyplanner/activities/MainActivity.kt
@@ -1,47 +1,42 @@
 package com.ggr3ml1n.dailyplanner.activities
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.recyclerview.widget.LinearLayoutManager
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.ggr3ml1n.dailyplanner.R
 import com.ggr3ml1n.dailyplanner.databinding.ActivityMainBinding
-import com.ggr3ml1n.dailyplanner.entities.Plan
-import com.ggr3ml1n.dailyplanner.recyclerview.PlanAdapter
 
 class MainActivity : AppCompatActivity() {
     
-    private lateinit var binding: ActivityMainBinding
-    private val adapter = PlanAdapter()
-    private lateinit var launcher: ActivityResultLauncher<Intent>
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
     
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        init()
-        launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
-            if (it.resultCode == RESULT_OK) {
-                
-                val name = it.data?.getStringExtra("plan.name")
-                val desc = it.data?.getStringExtra("plan.desc")
-                val plan = Plan(name!!, desc!!)
-                adapter.addPlan(plan)
-            }
-        }
+        
+        setBottomNavListener()
         
     }
     
-    fun init(){
-        binding.apply {
-            rView.layoutManager = LinearLayoutManager(this@MainActivity)
-            rView.adapter = adapter
-            bAddPlan.setOnClickListener {
-                launcher.launch(Intent(this@MainActivity, EditActivity::class.java))
+    private fun setBottomNavListener(){
+        binding.bottomMenu.setOnItemSelectedListener {
+            when (it.itemId){
+                R.id.btn_calendar -> {
+                    Toast.makeText(this, "Button Calendar has been pressed", Toast.LENGTH_SHORT).show()
+                }
+                R.id.plan_list -> {
+                    Toast.makeText(this, "Button List has been pressed", Toast.LENGTH_SHORT).show()
+                }
             }
+            true
         }
     }
+    
+    
+    
 }
+
 
