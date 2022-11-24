a7766b99e35abfb62810e0f133b30f6aaa48f65a
GGr3ml1N <dinars163@gmail.com>
2022-11-25 03:45:18 +0400

    Переназвал переменную для удобства

--- app/src/main/java/com/ggr3ml1n/dailyplanner/activities/MainApp.kt
+++ app/src/main/java/com/ggr3ml1n/dailyplanner/activities/MainApp.kt
@@ -4,5 +4,5 @@ import android.app.Application
 import com.ggr3ml1n.dailyplanner.database.MainDatabase
 
 class MainApp : Application() {
-    val dataBase by lazy { MainDatabase.getDatabase(this) }
+    val database by lazy { MainDatabase.getDatabase(this) }
 }
