01ae39790a2b29dde83806be77ffa8446f29d060
GGr3ml1N <dinars163@gmail.com>
2022-11-25 01:44:16 +0400

    Класс возвращен до исходного состояния, доработаю чуть позже :3

--- app/src/main/java/com/ggr3ml1n/dailyplanner/activities/EditActivity.kt
+++ app/src/main/java/com/ggr3ml1n/dailyplanner/activities/EditActivity.kt
@@ -1,10 +1,8 @@
 package com.ggr3ml1n.dailyplanner.activities
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.ggr3ml1n.dailyplanner.databinding.ActivityEditBinding
-import com.ggr3ml1n.dailyplanner.entities.Plan
 
 class EditActivity : AppCompatActivity() {
     
@@ -14,16 +12,5 @@ class EditActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityEditBinding.inflate(layoutInflater)
         setContentView(binding.root)
-    
-        binding.button.setOnClickListener {
-        
-            val plan = Plan(binding.edTitle.text.toString(), binding.edDesc.text.toString())
-            val editIntent = Intent(this, MainActivity::class.java).apply {
-                putExtra("plan.name", plan.name)
-                putExtra("plan.desc", plan.description)
-            }
-            setResult(RESULT_OK, editIntent)
-            finish()
-        }
     }
 }
