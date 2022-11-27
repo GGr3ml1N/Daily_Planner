877e6b90cc49729c5f6a2ac2357f9764c918e77b
GGr3ml1N <dinars163@gmail.com>
2022-11-28 02:05:54 +0400

    Переименовал, т.к. конфликтует с классом Calendar

--- app/src/main/java/com/ggr3ml1n/dailyplanner/fragments/CalendarFragment.kt
+++ app/src/main/java/com/ggr3ml1n/dailyplanner/fragments/CalendarFragment.kt
@@ -8,15 +8,17 @@ import androidx.fragment.app.activityViewModels
 import com.ggr3ml1n.dailyplanner.activities.MainApp
 import com.ggr3ml1n.dailyplanner.databinding.FragmentCalendarBinding
 import com.ggr3ml1n.dailyplanner.mainviewmodel.MainViewModel
+import java.text.SimpleDateFormat
+import java.util.*
 
 
-class Calendar : BaseFragment() {
+class CalendarFragment : BaseFragment() {
     
     private lateinit var binding: FragmentCalendarBinding
     private val mainViewModel: MainViewModel by activityViewModels {
         MainViewModel.MainViewModelFactory((context?.applicationContext as MainApp).database)
     }
-
+    
     
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,13 +28,26 @@ class Calendar : BaseFragment() {
         return binding.root
     }
     
-    override fun onClickNew() {
-        TODO("Not yet implemented")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.calendarView.setOnDateChangeListener { view, year, month, dayOfMonth ->
+            val selectedDay = SimpleDateFormat(
+                "dd MM yyyy",
+                Locale.getDefault(),
+            ).parse("$dayOfMonth ${month + 1} $year")
+                ?.time
+                .toString()
+            binding.tvChoose.text = selectedDay
+           mainViewModel.messageForListOfPlans.value = selectedDay
+        }
+        
     }
+    
+    override fun onClickNew() {}
     
     companion object {
         @JvmStatic
-        fun newInstance() = Calendar()
+        fun newInstance() = CalendarFragment()
         
     }
 }
